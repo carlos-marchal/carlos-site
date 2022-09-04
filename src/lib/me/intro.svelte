087cbfaa411190542d388d54b302e05476cd2c1a
Carlos Marchal <carlos.marchal@pm.me>
2022-09-04 18:31:26 +0200

    added about me page

---
+++ src/lib/me/intro.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import Logo from '$lib/logo.svelte';
+	import ScrollHijacker from '$lib/scroll-hijacker.svelte';
+</script>
+
+<ScrollHijacker height="400vh" threshold={0.25}>
+	<div class="logo">
+		<Logo />
+	</div>
+	<div class="content">
+		Hello! I'm Carlos Marchal. A Chief Technology Officer & Software Engineer who's passionate about
+		building software.
+	</div>
+	<div class="marquee">Born & raised in Barcelona</div>
+</ScrollHijacker>
+
+<style>
+	.logo {
+		position: absolute;
+		top: 50px;
+		left: 35px;
+	}
+
+	.content {
+		margin: 0 38px;
+		font-size: 22px;
+		text-align: center;
+	}
+
+	.marquee {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		line-height: 0.6;
+		text-transform: uppercase;
+		white-space: nowrap;
+		font-size: 52px;
+		transform: translateX(calc((100vw - 100%) * var(--progress)));
+	}
+
+	@media (min-width: 900px) {
+		.logo {
+			top: 100px;
+			left: 100px;
+		}
+
+		.content {
+			margin: 0 auto;
+			max-width: 1000px;
+			font-size: 35px;
+		}
+
+		.marquee {
+			font-size: 150px;
+		}
+	}
+</style>
---
+++ src/routes/me.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import Intro from '$lib/me/intro.svelte';
+</script>
+
+<Intro />
+<div style:height="200vh" />
