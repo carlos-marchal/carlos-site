97f0f3fb9d6332c45650b2cee54b35a0a7bb92d7
Carlos Marchal <carlos.marchal@pm.me>
2022-09-19 19:21:48 +0200

    added coming soon access wall

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
 	import '../app.css';
+	let taps = 0;
+	$: show = taps > 10;
 </script>
 
-<slot />
+{#if show}
+	<slot />
+{:else}
+	<div>Co<span on:click={() => taps++}>m</span>ing soon...</div>
+{/if}
+
+<style>
+	div {
+		margin-top: 50vh;
+		font-size: 50px;
+		text-align: center;
+		transform: translateY(-50%);
+		text-transform: uppercase;
+	}
+</style>
