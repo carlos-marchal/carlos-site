6552b617889f57c1f0043df8dbb9b9e012b6f9ae
Carlos Marchal <carlos.marchal@pm.me>
2022-10-09 11:28:40 +0200

    removed comign soon banner

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,27 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	let taps = 0;
-	$: show = taps >= 5 || import.meta.env.DEV;
 </script>
 
-{#if show}
-	<slot />
-{:else}
-	<div>Co<span on:click={() => taps++}>m</span>ing soon...</div>
-{/if}
-
-<style>
-	div {
-		margin-top: 50vh;
-		font-size: 50px;
-		text-align: center;
-		transform: translateY(-50%);
-		text-transform: uppercase;
-	}
-
-	@media (min-width: 800px) {
-		div {
-			font-size: 150px;
-		}
-	}
-</style>
+<slot />
