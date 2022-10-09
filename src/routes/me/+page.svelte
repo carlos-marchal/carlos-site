b96d60439d2212846f281afe62fffe5a6f52f8cc
Carlos Marchal <carlos.marchal@pm.me>
2022-10-09 11:34:12 +0200

    updated sveltekit and migrated routing layout

---
+++ src/routes/me/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Intro from './intro.svelte';
+	import Timeline from './timeline.svelte';
+	import Haddock from './haddock.svelte';
+	import YC from './yc.svelte';
+	import Skills from './skills.svelte';
+	import Footer from './footer.svelte';
+	import Languages from './languages.svelte';
+</script>
+
+<Intro />
+<Timeline />
+<Haddock />
+<YC />
+<Skills />
+<Languages />
+<Footer />
